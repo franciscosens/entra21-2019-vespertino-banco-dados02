610ec687c713878ec5279f82ff493c5e5f029d24
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2019-06-04 15:34:35 -0300

    Finalizado a  funcionalidade da edição do filme.

--- View/FilmeLista.Designer.cs
+++ View/FilmeLista.Designer.cs
@@ -52,6 +52,7 @@
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(767, 364);
             this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             // 
             // ColumnId
             // 
